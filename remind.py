8d0227e8292ae49b5f3ae69d21990676c9f57f54
Oskar Bodemyr <obodemyr@kth.se>
2014-02-14 13:06:35 +0900

    Added remind functions

---
+++ remind.py
@@ -0,0 +1,32 @@
+import pickle
+messages = {"^MERKLERN^":"Why would I need to remind my self?"}
+pickle.dump( messages, open ("messages.p","wb"))
+
+def pending(nick):
+	messages = pickle.load(open ("messages.p","rb"))
+	if nick in messages:
+		return True
+	else:
+		return False
+
+def add_message(nick,sender,message):
+	messages = pickle.load(open ("messages.p","rb"))
+	message = "Reminder from " + sender + " to " + nick + ": " + message
+	if nick in messages:
+		messages[nick].append(message)
+	else:
+		messages[nick] = [message]
+	pickle.dump( messages, open ("messages.p","wb"))
+
+def get_messages(nick):
+	messages = pickle.load(open ("messages.p","rb"))
+	messagelist = messages[nick]
+	del messages[nick]
+	pickle.dump( messages, open ("messages.p","wb"))
+	return messagelist
+
+add_message("Gurka","Sven","Det har ar ett meddelande")
+add_message("Gurka","Sven","Det har ar ett meddelande")
+print pending("Gurka")
+print get_messages("Gurka")
+print pending("Gurka")
