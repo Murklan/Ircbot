edb90afbe35f9b414a640015d8b9971da90e9048
Oskar Bodemyr <oskar.bodemyr@gmail.com>
2014-02-18 12:50:25 +0900

    Fix newline in wolfram

--- wolfram.py
+++ wolfram.py
@@ -5,6 +5,6 @@ client = wolframalpha.Client(botvariables.wolfram)
 def ask_wolfram(phrase):
 	res = client.query(phrase)
 	try:
-		return next(res.results).text
+		return (next(res.results).text).replace('\n',' ')
 	except:
 		return "No results found"
