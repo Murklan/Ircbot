9305ba41b25899933f8aaf44c52e55caeb2b78ee
Murklan <trattmurklan@gmail.com>
2015-12-08 19:51:38 +0100

    Somethingsomething broken

--- mtgsearch.py
+++ mtgsearch.py
@@ -14,22 +14,20 @@ def cardSearch(name):
     cardName = cardData[0]['name']
     cardInfo = cardName + ' : http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=' + str(cardData[0]['id'])
     cardSet = cardData[0]['cardSetName']   
-
     return [cardName, cardInfo, cardSet]
 
 
 
 def cardPrice(cardName, expansion):
 
-
 	cardSet = expansion.replace(' ', '+')
 	page = requests.get('https://www.magiccardmarket.eu/Products/Singles/' + cardSet + '/' + cardName)
 	tree = html.fromstring(page.text)
-
-	priceFrom = tree.xpath(u'//*[@id="siteContents"]/div/div[3]/div[1]/div[2]/table/tbody/tr[2]/td[2]/span[1]')[0].text
-	priceAvg = tree.xpath(u'//*[@id="siteContents"]/div/div[3]/div[1]/div[2]/table/tbody/tr[3]/td[2]')[0].text
+	
+	priceFrom = tree.xpath(u'//*[@id="ProductInformation"]/div/div[1]/div[1]/table/tbody/tr[2]/td[2]/span[1]')[0].text
+	priceAvg = tree.xpath(u'//*[@id="ProductInformation"]/div/div[1]/div[1]/table/tbody/tr[3]/td[2]')[0].text
 	try:
-		priceFoil = tree.xpath(u'//*[@id="siteContents"]/div/div[3]/div[1]/div[2]/table/tbody/tr[5]/td[2]')[0].text
+		priceFoil = tree.xpath(u'//*[@id="ProductInformation"]/div/div[1]/div[1]/table/tbody/tr[5]/td[2]')[0].text
 	except:
 		priceFoil = u'N/A'
 	return [priceFrom, priceAvg, priceFoil]
